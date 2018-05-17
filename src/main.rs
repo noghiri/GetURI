7ec3cdf12c91260a271fae8f55b0a5f3343de521
noghiri <noghiri@gmail.com>
2018-05-17 15:59:16 -0700

    updated comments for niceness
    BLARG

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,14 @@ fn main() {
 
 fn generate_uri() -> String {
     let mut output_uri = String::from(r""); //Compiler complains about this, but without it, everything breaks. idk why.
-    for arg in env::args().skip(1) {        //takes envarg in slot 1. slot 0 is the path to the executable itself. If you pass it multiple envargs, it'll cycle through - useful if you have an aftermarket clipboard. Someone should make sure that works.
+    for arg in env::args().skip(1) {        //takes envarg in slot 1. slot 0 is the path to the executable itself. If you
+                                            //pass it multiple envargs, it'll cycle through - useful if you have an 
+                                            //aftermarket clipboard. Someone should make sure that works.
         let mut uri_string = &arg;
         output_uri = uri_string.replace("\"", "/");
-        output_uri = output_uri.replace(" ", "%20");  //these three lines transform something like \\server\the share\the program\program.exe to file://server/the%20share/the%20program/program.exe which works better for this application.
+        output_uri = output_uri.replace(" ", "%20");  //these three lines transform something like 
+                                                      //\\server\the share\the program\program.exe to 
+                                                      //file://server/the%20share/the%20program/program.exe which works better for this application.
         output_uri.insert_str(0, "file:");
     }
     output_uri
